b7e43a15978b2eae5a479b80a3ccdf7f46d16f98
Pankaj Chauhan <codyofficial84@gmail.com>
2026-08-10 14:50:38 +0530

    Add src/lib/firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbnlNA0Sw91yArHWLJdk892ZPIVn-RX7I",
+  authDomain: "d2-01-0001.firebaseapp.com",
+  projectId: "d2-01-0001",
+  storageBucket: "d2-01-0001.firebasestorage.app",
+  messagingSenderId: "404376718009",
+  appId: "1:404376718009:web:8bc13011ffd9b2a6582968",
+  measurementId: "G-FDED9Q441P"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, db, googleProvider };
